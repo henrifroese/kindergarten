be2d631e6914bb0a44ae35019107aab7504b56a6
Henri Froese <henri.froese@yahoo.com>
2022-08-06 13:06:27 +0200

    Bump version: 0.0.2 → 0.0.3

--- kindergarten/__init__.py
+++ kindergarten/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Henri Froese"""
 __email__ = "henri.froese@yahoo.com"
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 from kindergarten.core import plot
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-version='0.0.2'
+version='0.0.3'
 
 requirements = [
     "dash-bootstrap-components>=1.2.0",
